dc5e9463c66fe83a04ffcf975208384a6defe6e0
anbndct <160716769+anbndct@users.noreply.github.com>
2024-11-04 07:36:28 +0700

    Update streamzema.py
    pastel yellow is a bad idea

--- streamzema.py
+++ streamzema.py
@@ -39,7 +39,7 @@ page_styles = {
     "Chatbot": """
         <style>
             [data-testid="stAppViewContainer"] {
-                background: linear-gradient(to right, #fff9e6, #f7e7b6);
+                background: linear-gradient(to right, #e0f7df, #b2dfdb);
                 color: white;
             }
         </style>
